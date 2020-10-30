5f058f5b7b32a08423def1392af2205637af917f
Franco Ezequiel Albornoz <franco.ezequiel@outlook.com>
2020-10-30 00:01:28 -0300

    hojas en blanco

---
+++ src/aivon/modelos/RevendedorData.java
@@ -0,0 +1,15 @@
+package aivon.modelos;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class RevendedorData {
+    
+}
