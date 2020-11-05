16ba3ed5e8761b00a5ff0bed17d9e10fdeb1f758
Franco Ezequiel Albornoz <franco.ezequiel@outlook.com>
2020-11-04 22:23:16 -0300

    Actualizado PedidoData.java

--- src/aivon/modelos/PedidoData.java
+++ src/aivon/modelos/PedidoData.java
@@ -40,10 +40,10 @@ public class PedidoData {
                 System.out.println("Pedido iniciado");
                 JOptionPane.showMessageDialog(null, "Pedido iniciado");
             } else {
-                System.out.println("No se pudo iniciar pedido");
-                JOptionPane.showMessageDialog(null, "No se pudo iniciar pedido");
+                System.out.println("No se pudo iniciar pedido. Ya hay un pedido de ese revendedor para esta campaña");
+                JOptionPane.showMessageDialog(null, "No se pudo iniciar pedido. Ya hay un pedido de ese revendedor para esta campaña");
             }
-            // CARGANDO ID GENERADA EN TABLA AL ALUMNO EN JAVA
+            // CARGANDO ID GENERADA EN TABLA AL PEDIDO EN JAVA
             if (llaves.next()) {
                 pedido.setId_pedido(llaves.getInt(1));
             }
@@ -77,8 +77,32 @@ public class PedidoData {
         return costo;
     }
 //##############################################################################
-//################### COSTO PEDIDO PÚBLICO #####################################
-    public double costoPedidoPublico(Pedido pedido) {
+//################ COSTO PEDIDO ACTIVO #########################################    
+    public double costoPedidoActivo(Pedido pedido) {
+
+        double costo = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT SUM(costo_caja)\n"
+                    + "AS costo_pedido\n"
+                    + "FROM caja_pedido\n"
+                    + "WHERE pedido.activo=1\n"
+                    + "AND id_pedido=" + pedido.getId_pedido() + ";");
+
+            costo = consulta.getDouble("costo_pedido");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+
+        return costo;
+    }
+//##############################################################################
+//################### COSTO PÚBLICO PEDIDO #####################################
+    public double costoPublicoPedido(Pedido pedido) {
 
         double costo = 0;
 
@@ -99,6 +123,97 @@ public class PedidoData {
 
         return costo;
     }
+//##############################################################################
+//################### COSTO PÚBLICO PEDIDO ACTIVO ##############################
+    public double costoPublicoPedidoActivo(Pedido pedido) {
+
+        double costo = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT SUM(costo_caja_publico)\n"
+                    + "AS costo_pedido\n"
+                    + "FROM caja_pedido\n"
+                    + "WHERE pedido.activo=1\n"
+                    + "AND id_pedido=" + pedido.getId_pedido() + ";");
+
+            costo = consulta.getDouble("costo_pedido");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+
+        return costo;
+    }
+//##############################################################################
+//################ COSTO PEDIDOS PAGOS X REVENDEDOR ############################
+    public double costoPedidosPagosRevendedor(Revendedor revendedor) {
+
+        double costo = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT SUM(costo_caja) AS costo_pedidos "
+                    + "FROM caja_pedido, pedido WHERE caja_pedido.id_pedido = pedido.id_pedido "
+                    + "AND pedido.fecha_pago IS NOT NULL AND pedido.id_revendedor=" + revendedor.getId_revendedor() + ";");
+
+            costo = consulta.getDouble("costo_pedidos");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+
+        return costo;
+    }
+//##############################################################################
+//################ COSTO PÚBLICO PEDIDOS PAGOS X REVENDEDOR ####################    
+    public double costoPublicoPedidosPagosRevendedor(Revendedor revendedor) {
+
+        double costo = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT SUM(costo_caja_publico) AS costo_pedidos "
+                    + "FROM caja_pedido, pedido WHERE caja_pedido.id_pedido = pedido.id_pedido "
+                    + "AND pedido.fecha_pago IS NOT NULL AND pedido.id_revendedor=" + revendedor.getId_revendedor() + ";");
+
+            costo = consulta.getDouble("costo_pedidos");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+
+        return costo;
+    }
+//##############################################################################
+//######################## GANANCIA TOTAL X REVENDEDOR #########################    
+//
+//    public double gananciaFinalRevendedor(Revendedor revendedor) {
+//
+//        double ganancia = 0;
+//
+//        try {
+//
+//            Statement statement = c.createStatement();
+//            ResultSet consulta = statement.executeQuery("SELECT SUM(costo_caja_publico) - SUM(costo_caja) "
+//                    + "AS ganancia FROM caja_pedido, pedido WHERE caja_pedido.id_pedido=pedido.id_pedido "
+//                    + "AND pedido.fecha_pago IS NULL AND pedido.id_revendedor=" + revendedor.getId_revendedor() + ";");
+//
+//            ganancia = consulta.getDouble("ganancia");
+//
+//        } catch (SQLException e) {
+//            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+//            System.out.println(e.getMessage());
+//        }
+//
+//        return ganancia;
+//    }    
 //##############################################################################
 //################ CANTIDAD ESTRELLAS PEDIDO ###################################
     public int cantEstrellasPedido(Pedido pedido) {
@@ -123,8 +238,54 @@ public class PedidoData {
         return cantidad;
     }
 //##############################################################################
-//################ CANTIDAD DE CAJAS DE PEDIDO #################################
+//################ CANTIDAD ESTRELLAS PEDIDO ACTIVO ############################
+    public int cantEstrellasPedidoActivo(Pedido pedido) {
 
+        int cantidad = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT SUM(estrellas_caja)\n"
+                    + "AS estrellas\n"
+                    + "FROM caja_pedido, pedido\n"
+                    + "WHERE pedido.activo=1\n"
+                    + "AND caja_pedido.id_pedido=" + pedido.getId_pedido() + ";");
+
+            cantidad = consulta.getInt("estrellas");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+        pedido.setEstrellas_pedido(cantidad);
+        return cantidad;
+    }
+//##############################################################################
+//################ COSTO PEDIDOS PAGOS X REVENDEDOR ############################
+    public double cantEstrellasPedidosPagoRevendedor(Revendedor revendedor) {
+
+        int estrellas = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT SUM(estrellas_caja) AS estrellas "
+                    + "FROM caja_pedido, pedido WHERE caja_pedido.id_pedido = pedido.id_pedido "
+                    + "AND pedido.fecha_pago IS NOT NULL AND pedido.id_revendedor=" 
+                    + revendedor.getId_revendedor() + ";");
+
+            estrellas = consulta.getInt("estrellas");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+
+        return estrellas;
+    }
+//##############################################################################
+//################ CANTIDAD DE CAJAS DE PEDIDO #################################
     public int cantCajasPedido(Pedido pedido) {
 
         int cantidad = 0;
@@ -147,7 +308,31 @@ public class PedidoData {
         pedido.setCantidad_cajas(cantidad);
         return cantidad;
     }
+//##############################################################################
+//################### CANTIDAD DE CAJAS DE PEDIDO ACTIVO #######################
+    public int cantCajasPedidoActivo(Pedido pedido) {
 
+        int cantidad = 0;
+
+        try {
+
+            Statement statement = c.createStatement();
+            ResultSet consulta = statement.executeQuery("SELECT COUNT(*) "
+                    + "AS cajas "
+                    + "FROM caja_pedido, pedido "
+                    + "WHERE pedido.activo=1\n"
+                    + "AND caja_pedido.id_pedido="
+                    + pedido.getId_pedido() + ";");
+
+            cantidad = consulta.getInt("cajas");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al realizar la consulta");
+            System.out.println(e.getMessage());
+        }
+        pedido.setCantidad_cajas(cantidad);
+        return cantidad;
+    }
 //##############################################################################
 //################ ACTUALIZAR FECHA DE ENTREGA #################################
     public void actualizarFechaEntrega(Pedido pedido, LocalDate fecha_entrega) {
@@ -172,6 +357,7 @@ public class PedidoData {
     }
 //##############################################################################
 //################### ACTUALIZAR FECHA DE PAGO #################################
+
     public void actualizarFechaPago(Pedido pedido, LocalDate fecha_pago) {
         // DEBERÍAMOS INFERIR EL ATRIBUTO PAGO(bool) DE LA BD
         try {
@@ -194,6 +380,7 @@ public class PedidoData {
     }
 //##############################################################################
 //################### DESACTIVAR/CANCELAR PEDIDO ###############################
+
     public void inhabilitarPedido(Pedido pedido) {
         // DEBERÍAMOS INFERIR EL ATRIBUTO PAGO(bool) DE LA BD
         try {
@@ -215,6 +402,7 @@ public class PedidoData {
     }
 //##############################################################################
 //############################# HABILITAR PEDIDO ###############################
+
     public void habilitarPedido(Pedido pedido) {
         // DEBERÍAMOS INFERIR EL ATRIBUTO PAGO(bool) DE LA BD
         try {
