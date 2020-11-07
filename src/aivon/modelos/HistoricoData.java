881b91d8159c5f42414007426e3d5fc4b866d293
necopolis <39461844+necopolis@users.noreply.github.com>
2020-11-07 00:52:55 -0300

    Modificacion de Historico y prueba en main.

--- src/aivon/modelos/HistoricoData.java
+++ src/aivon/modelos/HistoricoData.java
@@ -21,24 +21,26 @@ import javax.swing.JOptionPane;
  */
 public class HistoricoData {
     private Connection con;
+    private Conexion conexion;
 
     public HistoricoData(Conexion conexion) {
         this.con = conexion.getConnection();
+        this.conexion=conexion;
     }
     
    public void altaHistorico(Historico historico){
-       String pre_instruccion = "INSERT INTO historico (id_revendedor, id_campaña, monto_min, monto_max, estado_campaña_revendedor) VALUES (?, ?, ?, ?, ?);";
+       String pre_instruccion = "INSERT INTO historico (id_revendedor, id_campaña, monto_min, monto_max, nivel, ganancia, estrellas_campaña_revendedor) VALUES (?, ?, ?, ?, ?, ?, ?);";
 
         try {
             PreparedStatement instruccion = con.prepareStatement(pre_instruccion, Statement.RETURN_GENERATED_KEYS);
-
             instruccion.setInt(1, historico.getRevendedor().getId_revendedor());
             instruccion.setInt(2, historico.getCampaña().getId_campaña());
             instruccion.setDouble(3, historico.getMonto_min());
-
             instruccion.setDouble(4, historico.getMonto_max());
             //El estado campaña revendedor es cuando pasada 3 campañas se desactiva 
-            instruccion.setBoolean(5, historico.isEstado_campaña_revendedor());
+            instruccion.setInt(5, historico.getNivel());
+            instruccion.setDouble(6, historico.getGanancia());
+            instruccion.setInt(7, historico.getEstrellas_campaña_revendedor());
             
 
             int celAfectadas = instruccion.executeUpdate();
@@ -51,7 +53,7 @@ public class HistoricoData {
                 System.out.println("No se pudo cargar historico");
                 JOptionPane.showMessageDialog(null, "No se pudo cargar historico");
             }
-            // CARGANDO ID GENERADA EN TABLA AL ALUMNO EN JAVA
+            // CARGANDO ID GENERADA EN TABLA AL HISTORICO EN JAVA
             if (llaves.next()) {
                 historico.setId_historico(llaves.getInt(1));
             }
@@ -59,7 +61,7 @@ public class HistoricoData {
             instruccion.close();
 
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Error al cargar pedido del producto");
+            JOptionPane.showMessageDialog(null, "Error al cargar Historico");
             System.out.println(e.getMessage());
         }
    
@@ -79,5 +81,33 @@ public class HistoricoData {
    public void listaHistoricoDeCampañas(){
        //como fue tal campaña, listar todos los revendedores de tal campaña
    }
-
+   
+   public Revendedor buscarRevendedor(String dni){
+       RevendedorData rd= new RevendedorData(conexion);
+        return rd.buscarRevendedor(dni);
+   }
+   public Campaña buscarCampaña(int id){
+       CampañaData cd=new CampañaData(conexion);
+       return cd.buscarCampaña(id);
+   }
+   public int cantidadEstrellasCampaña(Revendedor revendedor, Campaña campaña){
+       PedidoData pd=new PedidoData(conexion);
+       return pd.buscarPedido(revendedor.getId_revendedor(), campaña.getId_campaña()).getEstrellas_pedido() ;
+   }
+   public double gananciaRevendedorCampaña(Revendedor revendedor, Campaña campaña){
+       RevendedorData rd= new RevendedorData(conexion);
+       return rd.gananciaRevendedorPorCampaña(revendedor, campaña);
+   } 
+   public int nivelRevendedor(Revendedor revendedor){
+       RevendedorData rd=new RevendedorData(conexion);
+       return rd.calcularNivelRevendedor(revendedor);
+    }
+   public double montoMinRevendedor(Revendedor revendedor){
+       RevendedorData rd=new RevendedorData(conexion);
+       return rd.calcularMontoMinimoRevendedor(revendedor);
+   }
+   public double montoMaxRevendedor(Revendedor revendedor){
+       RevendedorData rd=new RevendedorData(conexion);
+       return rd.calcularMontoMaximoRevendedor(revendedor);
+   }
 }
