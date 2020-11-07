833d80c963f190d67465212dab9488d9e765f221
Genaro Farias <Genaro@Genaro-Laptop567>
2020-11-07 00:59:23 -0300

    Pruebas en main y mod en revendedor java

--- src/aivon/modelos/RevendedorData.java
+++ src/aivon/modelos/RevendedorData.java
@@ -350,9 +350,12 @@ public class RevendedorData {
         List<Revendedor> revendedores = new ArrayList<>();
 
         try {
-            PreparedStatement instruccion = con.prepareStatement("SELECT revendedor.id_revendedor, nombre, apellido, dni, tel, email, "
-                    + "revendedor.activo FROM revendedor, historico,campaña  where historico.id_campaña = campaña.id_campaña AND  "+
-                    "revendedor.id_revendedor= historico.id_revendedor AND campaña.id_campaña =" + idCampaña);
+            PreparedStatement instruccion = con.prepareStatement("SELECT revendedor.id_revendedor, "
+                    + "revendedor.nombre, apellido, dni, tel, email, "
+                    + "revendedor.activo FROM revendedor,campaña  "
+                    + "where historico.id_campaña = campaña.id_campaña AND  "
+                    + "revendedor.id_revendedor= historico.id_revendedor "
+                    + "AND campaña.id_campaña =" + idCampaña);
 
             ResultSet consulta = instruccion.executeQuery();
             
@@ -401,10 +404,12 @@ public class RevendedorData {
             Statement statement = con.createStatement();
             ResultSet consulta = statement.executeQuery("SELECT SUM(costo_caja_publico) - SUM(costo_caja) "
                     + "AS ganancia FROM caja_pedido, pedido WHERE caja_pedido.id_pedido=pedido.id_pedido "
-                    + "AND pedido.fecha_pago IS NULL AND pedido.id_revendedor=" + revendedor.getId_revendedor() + ";");
+                    + "AND pedido.fecha_pago IS NOT NULL AND pedido.id_revendedor=" 
+                    + revendedor.getId_revendedor() + ";");
 
+            if(consulta.next()){
             ganancia = consulta.getDouble("ganancia");
-
+            }
            statement.close();
            
         } catch (SQLException e) {
@@ -429,8 +434,9 @@ public class RevendedorData {
                     + "AND pedido.fecha_pago IS NULL AND pedido.id_revendedor=" + revendedor.getId_revendedor() 
                     + " AND pedido.id_pedido ="+ pedido.getId_pedido()+";");
 
+            if(consulta.next()){
             ganancia = consulta.getDouble("ganancia");
-
+            }
             statement.close();
             
         } catch (SQLException e) {
@@ -459,8 +465,9 @@ public class RevendedorData {
                     + "AND pedido.id_revendedor =" + revendedor.getId_revendedor() 
                     + "AND campaña.id_campaña =" + campaña.getId_campaña()+";");
 
+            if(consulta.next()){
             ganancia = consulta.getDouble("ganancia");
-
+            }
             statement.close();
             
         } catch (SQLException e) {
@@ -488,8 +495,9 @@ public class RevendedorData {
                     + "AND pedido.id_revendedor =" + revendedor.getId_revendedor() 
                     +";");
 
+            if(consulta.next()){
             estrellas = consulta.getInt("estrellas");
-
+            }
             statement.close();
             
         } catch (SQLException e) {
@@ -521,8 +529,15 @@ public class RevendedorData {
                     + "AND pedido.id_revendedor =" + revendedor.getId_revendedor() 
                     +";");
 
+            if(consulta.next()){
             nivel = consulta.getInt("nivel");
-            
+            revendedor.setNivel(nivel);
+            }else{
+                JOptionPane.showMessageDialog(null, "No se obtuvo nivel");
+                System.out.println("No se obtuvo el nivel");
+            }
+                
+            statement.close();
             int celAfectadas = statement.executeUpdate("Update revendedor Set nivel = "+nivel+" where id_revendedor = "+revendedor.getId_revendedor()+";");
             if (celAfectadas > 0) {
                 System.out.println("Nivel actualizado");
